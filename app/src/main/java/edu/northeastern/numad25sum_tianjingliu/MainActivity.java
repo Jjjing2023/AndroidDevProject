2ed43e5ff3da5ac9a4deb19ac60c2974538abb53
Jjjing2023 <tl2671@tc.columbia.edu>
2025-05-19 09:53:56 -0700

    add new activity using intent

---
+++ app/src/main/java/edu/northeastern/numad25sum_tianjingliu/MainActivity.java
@@ -0,0 +1,54 @@
+package edu.northeastern.numad25sum_tianjingliu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityAboutMe.class);
+                startActivity(intent);
+            }
+
+
+        });
+    }}
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//    }
+
+//    public void showInformation(View view){
+//
+//        CharSequence text = "name: Tianjing Liu\nemail: liu.tianj@northeastern.edu";
+//        int duration = Toast.LENGTH_SHORT;
+//
+//        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+//        toast.show();
+//
+//    }
+
