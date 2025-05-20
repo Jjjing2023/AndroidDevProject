01b89f20c6cb1e39a16d6abffcefcaa6642cd698
Jjjing2023 <tl2671@tc.columbia.edu>
2025-05-20 10:18:42 -0700

    added the [QuicCalc] button

--- app/src/main/java/edu/northeastern/numad25sum_tianjingliu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad25sum_tianjingliu/MainActivity.java
@@ -15,11 +15,11 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-
+    private Button quicCalc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
@@ -29,26 +29,23 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, ActivityAboutMe.class);
                 startActivity(intent);
             }
-
-
         });
-    }}
+
+        quicCalc = findViewById(R.id.calc);
+        quicCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuicCalc.class);
+                startActivity(intent);
+            }
+        });
 
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
+    }
 
-//    public void showInformation(View view){
-//
-//        CharSequence text = "name: Tianjing Liu\nemail: liu.tianj@northeastern.edu";
-//        int duration = Toast.LENGTH_SHORT;
-//
-//        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-//        toast.show();
-//
-//    }
+
+
+
+
+}
 
