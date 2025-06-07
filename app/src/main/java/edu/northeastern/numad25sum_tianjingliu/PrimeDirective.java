3996f5bac764e5f24f29b404c544975df4152955
Jjjing2023 <tl2671@tc.columbia.edu>
2025-06-07 15:04:16 -0700

    completed search and terminate buttons

--- app/src/main/java/edu/northeastern/numad25sum_tianjingliu/PrimeDirective.java
+++ app/src/main/java/edu/northeastern/numad25sum_tianjingliu/PrimeDirective.java
@@ -1,6 +1,8 @@
 package edu.northeastern.numad25sum_tianjingliu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PrimeDirective extends AppCompatActivity {
+    private TextView currentText;
+    private TextView latestText;
+    private volatile boolean isSearching = false;
+    private Thread workerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.prime_directive);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        currentText = findViewById(R.id.current);
+        latestText = findViewById(R.id.latest);
+
     }
+
+    public void findPrimesClick(View view) {
+        if (isSearching) return;
+        isSearching = true;
+
+        workerThread= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 3;
+                while(isSearching){
+                    if(i%100 == 3){
+                        final int current = i;
+                        runOnUiThread(()->currentText.setText("Checking: " + current));
+                    }
+
+                    if(isPrime(i)){
+                        final int prime = i;
+
+                        runOnUiThread(()->latestText.setText("Latest Prime: "+prime));
+                    }
+                    i+=2;
+                }
+                runOnUiThread(() -> currentText.setText("Search stopped"));
+            }
+        });
+        workerThread.start();
+
+    }
+    public void terminateSearchClick(View view){
+        isSearching = false;
+    }
+    private boolean isPrime(int number){
+        for(int i = 2; i < number;i++){
+            if(number%i==0)return false;
+        }
+        return true;
+    }
+
 }
